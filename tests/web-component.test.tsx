5a94b9ee66e3ff8ff93ee38bf876d716d30d0e91
Hana <enuxura@gmail.com>
2023-11-18 19:52:31 +0100

    test: add more test on web components

--- tests/web-component.test.tsx
+++ tests/web-component.test.tsx
@@ -3,9 +3,11 @@ import type React from "react"
 
 import { test, expect, describe, vi, afterEach } from "vitest"
 import { define, h, render } from "../src/web"
-import { onMounted, onUnmounted } from "../src/app"
+import { isReactor, onMounted, onUnmounted } from "../src/app"
 
 import "./polyfill"
+import { Reactor } from "../types/app"
+import { next } from "../src/runtime"
 
 /** @jsx h */
 
@@ -79,11 +81,149 @@ test("passing the attribute", () => {
     expect(attributes).toMatchObject({
       a: "a",
       b: "0",
+      c: "",
     })
     expect(ref).not.toBeNull()
     return <p>Hello</p>
   }
   define("test-1", component)
   // @ts-ignore
+  render(<test-1 a="a" b={0} c></test-1>, document.body)
+})
+
+test("reactive attribute", () => {
+  let a: Reactor<string> | null = null
+  const component = ({ ref, ...attributes }) => {
+    expect(isReactor(attributes.a)).toBeTruthy()
+    expect(attributes.b).toBe("0")
+    expect(ref).not.toBeNull()
+
+    a = attributes.a
+
+    return <p>Hello</p>
+  }
+  define("test-1", component, {
+    reactiveAttributes: ["a"],
+  })
+
+  expect(a).toBeNull()
+  // @ts-ignore
   render(<test-1 a="a" b={0}></test-1>, document.body)
+  expect(a).not.toBeNull()
+  expect(a!()).toBe("a")
+
+  const test_el = document.querySelector("test-1")!
+  test_el.setAttribute("a", "b")
+  expect(a!()).toBe("b")
+})
+
+test("casting attribute", () => {
+  let i = 0
+  const component = ({ ref, ...attributes }) => {
+    if (i === 0)
+      expect(attributes).toMatchObject({
+        a: true,
+        b: 0,
+        c: true,
+      })
+    else if (i === 1)
+      expect(attributes).toMatchObject({
+        a: false,
+        b: 1,
+        c: false,
+      })
+
+    expect(ref).not.toBeNull()
+    return <p>Hello</p>
+  }
+  define("test-1", component, {
+    attributeCast: {
+      a(el: HTMLElement, value: string) {
+        return value == "a"
+      },
+      b: Number,
+      c: Boolean,
+    },
+  })
+  // @ts-ignore
+  render(<test-1 a="a" b={0} c></test-1>, document.body)
+  i++
+  // @ts-ignore
+  render(<test-1 a="b" b="1"></test-1>, document.body)
+})
+
+test("casted reactive attribute", async () => {
+  let a: Reactor<number> | null = null
+  let c: Reactor<number> | null = null
+  const component = ({ ref, ...attributes }) => {
+    expect(isReactor(attributes.a)).toBeTruthy()
+    expect(isReactor(attributes.c)).toBeTruthy()
+    expect(attributes.b).toBe("0")
+    expect(ref).not.toBeNull()
+
+    a = attributes.a
+    c = attributes.c
+
+    return <p>Hello</p>
+  }
+  define("test-1", component, {
+    reactiveAttributes: ["a", "c"],
+    attributeCast: {
+      a: Number,
+      c: Boolean,
+    },
+  })
+
+  expect(a).toBeNull()
+  expect(c).toBeNull()
+  // @ts-ignore
+  render(<test-1 a="0" b={0}></test-1>, document.body)
+  expect(a).not.toBeNull()
+  expect(c).not.toBeNull()
+  expect(a!()).toBe(0)
+  expect(c!()).toBeFalsy()
+
+  const test_el = document.querySelector("test-1")!
+  test_el.setAttribute("a", "2")
+  test_el.setAttribute("c", "a")
+
+  await next()
+
+  expect(a!()).toBe(2)
+  expect(c!()).toBeTruthy()
+})
+
+test("shadow dom open", () => {
+  const component = vi.fn(() => {
+    return <p>Hello</p>
+  })
+  define("test-1", component, {
+    shadowed: "open",
+  })
+
+  expect(component).toBeCalledTimes(0)
+  // @ts-ignore
+  render(<test-1></test-1>, document.body)
+  expect(component).toBeCalledTimes(1)
+  expect(document.body.innerHTML).toBe("<test-1></test-1>")
+  const test_el = document.querySelector("test-1")!
+  expect(test_el.shadowRoot).not.toBeNull()
+  expect(test_el.shadowRoot!.innerHTML).toBe("<p>Hello</p>")
+})
+
+test("shadow dom closed", () => {
+  const component = vi.fn(() => {
+    return <p>Hello</p>
+  })
+  define("test-1", component, {
+    shadowed: "closed",
+  })
+
+  expect(component).toBeCalledTimes(0)
+  // @ts-ignore
+  render(<test-1></test-1>, document.body)
+  expect(component).toBeCalledTimes(1)
+  expect(document.body.innerHTML).toBe("<test-1></test-1>")
+  const test_el = document.querySelector("test-1")!
+  expect(test_el.shadowRoot).toBeNull()
 })
